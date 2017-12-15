c29b1c9cc58faa41369bb3f32d1cc7bca0ba0dbf
Nikita Gordeev <gordeev.nik@mail.ru>
2017-12-15 14:58:12 +0300

    Lab works

--- Lab12.c
+++ Lab12.c
@@ -9,7 +9,8 @@ int addNumbers(int a, int b)
 
 int main(int argCount, char *argv[])
 {
-    freopen("../output.txt", "w", stdout);
+    printf("This Lab Should be opened in console with using >python Lab12.c");
+    freopen("../output.txt", "w", stdout);      // <--Output file will be created after running program.
 
     int first = atoi(argv[1]);
     int second = atoi(argv[2]);
